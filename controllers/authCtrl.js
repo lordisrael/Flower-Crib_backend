c3c8e0f2b8f45f6a87fb32baaa272dac409bd707
lordisrael <josephisrael206@gmail.com>
2023-11-01 13:27:08 +0100

    Added Login

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -3,7 +3,8 @@ const uuidValidate = require("uuid-validate");
 const { StatusCodes } = require("http-status-codes");
 const { Op } = require("sequelize");
 const sequelize = require("../db/database");
-
+const { createJWT } = require("../config/jwt");
+const { createRefreshJWT } = require("../config/refreshjwt");
 
 const createPerson = async (req, res) => {
   const { email } = req.body;
@@ -16,6 +17,43 @@ const createPerson = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (user && (await user.comparePassword(password))) {
+      const refreshToken = await createRefreshJWT(user.id);
+
+      // Update the user's refreshToken in the database
+      await user.update({ refreshToken }, { returning: true });
+
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 72 * 60 * 60 * 1000,
+      });
+
+      res.status(StatusCodes.OK).json({
+        id: user.id,
+        name: user.name, // Assuming "name" is a field in your User model
+        email: user.email,
+        token: createJWT(user.id, user.name),
+      });
+    } else {
+      res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ error: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal server error" });
+  }
+};
+
 module.exports = {
   createPerson,
+  login
 };
