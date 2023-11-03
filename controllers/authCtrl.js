cc10cb65cb808d06932daabd8279426a5ec73c5f
lordisrael <josephisrael206@gmail.com>
2023-11-03 09:42:26 +0100

    Editing

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -19,40 +19,73 @@ const createPerson = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
 
-  try {
-    const user = await User.findOne({ where: { email } });
+  if (user && (await user.comparePassword(password))) {
+    const refreshToken = await createRefreshJWT(user.id);
 
-    if (user && (await user.comparePassword(password))) {
-      const refreshToken = await createRefreshJWT(user.id);
+    // Update the user's refreshToken in the database
+    await user.update({ refreshToken }, { returning: true });
 
-      // Update the user's refreshToken in the database
-      await user.update({ refreshToken }, { returning: true });
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 72 * 60 * 60 * 1000,
+    });
 
-      res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        maxAge: 72 * 60 * 60 * 1000,
-      });
-
-      res.status(StatusCodes.OK).json({
-        status: "SUCCESS",
-        message: "Signin successful",
-        id: user.id,
-        name: user.name, // Assuming "name" is a field in your User model
-        email: user.email,
-        token: createJWT(user.id, user.name),
-      });
-    } else {
-      res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ status: "FAILED", message: "Invalid crendentials" });
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: "Internal server error" });
+    res.status(StatusCodes.OK).json({
+      status: "SUCCESS",
+      message: "Signin successful",
+      data: [
+        {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          token: createJWT(user.id, user.name),
+        },
+      ],
+    });
+  } else {
+    res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid credentials" });
   }
+
+
+  // try {
+  //   const user = await User.findOne({ where: { email } });
+
+  //   if (user && (await user.comparePassword(password))) {
+  //     const refreshToken = await createRefreshJWT(user.id);
+
+  //     // Update the user's refreshToken in the database
+  //     await user.update({ refreshToken }, { returning: true });
+
+  //     res.cookie("refreshToken", refreshToken, {
+  //       httpOnly: true,
+  //       maxAge: 72 * 60 * 60 * 1000,
+  //     });
+
+  //     res.status(StatusCodes.OK).json({
+  //       status: "SUCCESS",
+  //       message: "Signin successful",
+  //       data: [
+  //         {
+  //           id: user.id,
+  //           name: user.name,
+  //           email: user.email,
+  //           token: createJWT(user.id, user.name),
+  //         },
+  //       ],
+  //     });
+  //   } else {
+  //     res
+  //       .status(StatusCodes.UNAUTHORIZED)
+  //       .json({ error: "Invalid credentials" });
+  //   }
+  // } catch (error) {
+  //   console.error("Error:", error);
+  //   res
+  //     .status(StatusCodes.INTERNAL_SERVER_ERROR)
+  //     .json({ error: "Internal server error" });
+  // }
 };
 
 module.exports = {
